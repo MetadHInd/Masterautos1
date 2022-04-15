eae8d72660c03190b16880be95c1faa05d20948c
MetadHInd <72826930+MetadHInd@users.noreply.github.com>
2022-04-15 15:41:04 -0500

    Add files via upload

--- procesos.js
+++ procesos.js
@@ -1,6 +1,24 @@
 const navToggle = document.querySelector(".nav-toogle")
 const navMenu = document.querySelector(".nav-menu")
 
+document.addEventListener("keyup", e=>{
+
+	if (e.target.matches("#buscador")){
+  
+		if (e.key ==="Escape")e.target.value = ""
+  
+		document.querySelectorAll(".articulo").forEach(fruta =>{
+  
+			fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+			  ?fruta.classList.remove("filtro")
+			  :fruta.classList.add("filtro")
+		})
+  
+	}
+  
+  
+  })
+
 navToggle.addEventListener("click", () => 
 {
     navMenu.classList.toggle("nav-menu_visible")
@@ -34,4 +52,9 @@ window.addEventListener('load', function(){
 			}
 		]
 	});
+
 });
+
+
+
+
